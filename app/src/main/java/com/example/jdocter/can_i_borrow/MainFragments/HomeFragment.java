0badf08e5b4fd81105d6343f26f68818f034ff8a
Jordan E Docter <jdocter@fb.com>
2018-07-17 10:21:06 -0700

    commit gradle

--- app/src/main/java/com/example/jdocter/can_i_borrow/MainFragments/HomeFragment.java
+++ app/src/main/java/com/example/jdocter/can_i_borrow/MainFragments/HomeFragment.java
@@ -1,109 +1,83 @@
 package com.example.jdocter.can_i_borrow.MainFragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.jdocter.can_i_borrow.HomeFragments.BorrowFragment;
+import com.example.jdocter.can_i_borrow.HomeFragments.CalenderFragment;
+import com.example.jdocter.can_i_borrow.HomeFragments.LendFragment;
+import com.example.jdocter.can_i_borrow.HomeFragments.MessageFragment;
 import com.example.jdocter.can_i_borrow.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    // define your fragments here
+    final Fragment borrowFragment = new BorrowFragment();
+    final Fragment calenderFragment = new CalenderFragment();
+    final Fragment lendFragment = new LendFragment();
+    final Fragment messageFragment = new MessageFragment();
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    private BottomNavigationView bottomNavigationView;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_home, parent, false);
+
+
     }
 
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        final FragmentManager homeFragmentManager = getActivity().getSupportFragmentManager();
+
+        bottomNavigationView = (BottomNavigationView) view.findViewById(R.id.bottom_navigation);
+
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        FragmentTransaction fragmentTransaction = homeFragmentManager.beginTransaction();
+                        switch (item.getItemId()) {
+                            case R.id.borrow:
+                                fragmentTransaction.replace(R.id.homeContainer, borrowFragment).commit();
+                                return true;
+                            case R.id.lend:
+                                fragmentTransaction.replace(R.id.homeContainer, lendFragment).commit();
+                                return true;
+                            case R.id.calender:
+                                fragmentTransaction.replace(R.id.homeContainer, calenderFragment).commit();
+                                return true;
+                            case R.id.message:
+                                fragmentTransaction.replace(R.id.homeContainer, messageFragment).commit();
+                                return true;
+                        }
+                        return true; // TODO this supposed to be here?
+                    }
+                }
+        );
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
