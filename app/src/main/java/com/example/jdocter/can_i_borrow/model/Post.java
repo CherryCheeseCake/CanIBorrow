0039e639e8e208d01492a565766918fb1c3d1344
Jordan E Docter <jdocter@fb.com>
2018-07-18 10:17:23 -0700

    ParseApp an Post model

---
+++ app/src/main/java/com/example/jdocter/can_i_borrow/model/Post.java
@@ -0,0 +1,8 @@
+package com.example.jdocter.can_i_borrow.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+}
